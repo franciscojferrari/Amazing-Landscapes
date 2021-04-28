0f497fcfd8a6cedda9e3101a940f4d1e5e132247
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-04-28 19:46:51 +0200

    fix discriminator

--- discriminator/Discriminator.py
+++ discriminator/Discriminator.py
@@ -7,68 +7,60 @@ from typing import Any
 
 class Discriminator(tfkl.Layer):
     def __init__(
-        self,
-        input_nc: int,
-        ndf: int = 64,
-        n_layers: int = 3,
-        norm_layer: Any = tfkl.BatchNormalization,
-        name="discriminator",
-        **kwargs,
+            self,
+            name="discriminator",
+            **kwargs,
     ):
         super(Discriminator, self).__init__(name=name, **kwargs)
-        """PatchGAN discriminator
-        
-        Inputs:
-            input_nc: The number of channels in input images.
-            ndf: The number of filters in the last conv layer.
-            n_layers: The number of conv layers in the discriminator.
-            norm_layer: Normalization layer.
-        """
+        """PatchGAN discriminator"""
+        self.leakyrelu = tfkl.LeakyReLU(alpha=0.2)
+        self.layer1 = tfkl.Conv2D(
+            filters=64,
+            kernel_size=4,
+            strides=2,
+            padding="same",
+            activation=tfkl.LeakyReLU(alpha=0.2),
+        )
 
-        kernel_size = 4
-        use_bias = False  # This parameter depends on the normalisation used.
-        nf = ndf
+        self.layer2 = tfkl.Conv2D(
+            filters=128,
+            kernel_size=4,
+            strides=2,
+            padding="same",
+        )
+        self.norm2 = tfa.layers.InstanceNormalization(groups=128)
 
-        sequence = [
-            tfkl.Conv2D(
-                filters=input_nc,
-                kernel_size=kernel_size,
-                strides=2,
-                padding="same",
-                activation=tfkl.LeakyReLU(alpha=0.2),
-            )
-        ]
+        self.layer3 = tfkl.Conv2D(
+            filters=256,
+            kernel_size=4,
+            strides=2,
+            padding="same",
+        )
+        self.norm3 = tfa.layers.InstanceNormalization(groups=256)
 
-        for n in range(1, n_layers):  # gradually increase the number of filters
-            nf = min(2 * nf, 512)
-            stride = 1 if n == n_layers - 1 else 2
-            sequence += [
-                norm_layer(
-                    tfkl.Conv2D(
-                        filters=nf,
-                        kernel_size=kernel_size,
-                        strides=stride,
-                        padding="same",
-                        use_bias=use_bias,
-                    )
-                ),
-                tfa.layers.InstanceNormalization(groups=nf),
-                tfkl.LeakyReLU(alpha=0.2),
-            ]
+        self.layer4 = tfkl.Conv2D(
+            filters=512,
+            kernel_size=4,
+            strides=2,
+            padding="same",
+        )
+        self.norm4 = tfa.layers.InstanceNormalization(groups=512)
 
-        # nf = min(2 * n_layers, 512)
-        # sequence += [
-        #     tfkl.Conv2D(filters=nf, kernel_size=kernel_size, strides=1, padding="same",
-        #                 use_bias=use_bias),
-        #     tfkl.LeakyReLU(alpha=0.2),
-        # ]
-
-        sequence += [
-            tfkl.Conv2D(filters=1, kernel_size=kernel_size, strides=1, padding="same")
-        ]  # output 1 channel prediction map
-        self.model = tf.keras.Sequential(layers=sequence)
+        self.output_layer = tfkl.Conv2D(
+            filters=1,
+            kernel_size=4,
+            strides=1,
+            padding="same")
 
     def call(self, image, mask):
 
-        inputs = tf.concat([image, mask], axi=1)
-        return self.model(inputs)
+        inputs = tf.concat([image, mask], axis=-1)
+
+        x = self.layer1(inputs)
+        x = self.leakyrelu(self.norm2(self.layer2(x)))
+        x = self.leakyrelu(self.norm3(self.layer3(x)))
+        x = self.leakyrelu(self.norm4(self.layer4(x)))
+
+        out = self.output_layer(x)
+
+        return out
