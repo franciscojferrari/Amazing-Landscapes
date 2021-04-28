c1f223a442abedf84ad0783fb881cc8bfbaea1e5
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-04-28 11:51:41 +0200

    styling and added normalization layer

--- discriminator/Discriminator.py
+++ discriminator/Discriminator.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 import tensorflow.keras.layers as tfkl
+import tensorflow_addons as tfa
 
 from typing import Any
 
 
 class Discriminator(tfkl.Layer):
     def __init__(
-        self, input_nc: int, ndf: int = 64, n_layers: int = 3, norm_layer: Any =tfkl.BatchNormalization, name="discriminator", **kwargs,
+        self,
+        input_nc: int,
+        ndf: int = 64,
+        n_layers: int = 3,
+        norm_layer: Any = tfkl.BatchNormalization,
+        name="discriminator",
+        **kwargs,
     ):
         super(Discriminator, self).__init__(name=name, **kwargs)
         """PatchGAN discriminator
@@ -20,32 +27,48 @@ class Discriminator(tfkl.Layer):
 
         kernel_size = 4
         use_bias = False  # This parameter depends on the normalisation used.
+        nf = ndf
 
-        sequence = [tfkl.Conv2D(
-            filters=input_nc, kernel_size=kernel_size, strides=2, padding='same', activation=tfkl.LeakyReLU(alpha=0.2)
-        )]
+        sequence = [
+            tfkl.Conv2D(
+                filters=input_nc,
+                kernel_size=kernel_size,
+                strides=2,
+                padding="same",
+                activation=tfkl.LeakyReLU(alpha=0.2),
+            )
+        ]
 
         for n in range(1, n_layers):  # gradually increase the number of filters
-            nf_mult = min(2 ** n, 8)  # TODO: Check where to start here.
+            nf = min(2 * nf, 512)
+            stride = 1 if n == n_layers - 1 else 2
             sequence += [
-                tfkl.Conv2D(filters=ndf * nf_mult, kernel_size=kernel_size, strides=2, padding="same", use_bias=use_bias),
-                norm_layer(ndf * nf_mult),
+                norm_layer(
+                    tfkl.Conv2D(
+                        filters=nf,
+                        kernel_size=kernel_size,
+                        strides=stride,
+                        padding="same",
+                        use_bias=use_bias,
+                    )
+                ),
+                tfa.layers.InstanceNormalization(groups=nf),
                 tfkl.LeakyReLU(alpha=0.2),
             ]
 
-        nf_mult = min(2 ** n_layers, 8)
-        sequence += [
-            tfkl.Conv2D(filters=ndf * nf_mult, kernel_size=kernel_size, strides=1, padding="same",
-                        use_bias=use_bias),
-            norm_layer(ndf * nf_mult),
-            tfkl.LeakyReLU(alpha=0.2),
-        ]
+        # nf = min(2 * n_layers, 512)
+        # sequence += [
+        #     tfkl.Conv2D(filters=nf, kernel_size=kernel_size, strides=1, padding="same",
+        #                 use_bias=use_bias),
+        #     tfkl.LeakyReLU(alpha=0.2),
+        # ]
 
-        sequence += [tfkl.Conv2D(filters=1, kernel_size=kernel_size, strides=1, padding="same")] # output 1 channel prediction map
+        sequence += [
+            tfkl.Conv2D(filters=1, kernel_size=kernel_size, strides=1, padding="same")
+        ]  # output 1 channel prediction map
         self.model = tf.keras.Sequential(layers=sequence)
 
-    def call(self, inputs):
+    def call(self, image, mask):
 
-        # TODO: add concatenation
-
+        inputs = tf.concat([image, mask], axi=1)
         return self.model(inputs)
