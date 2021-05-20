42930365480f1d8485a86a1c1041ada6c091ab14
annasanchez27 <annasanchezespunyes27@gmail.com>
2021-05-20 18:08:36 +0200

    Updated wrong FID and pixel accuracy

--- metrics/pixelacc.py
+++ metrics/pixelacc.py
@@ -11,17 +11,15 @@ def pixel_accuracy(eval_segm, gt_segm):
     Returns:
         float: pixel accuracy
     """
-
     assert eval_segm.shape == gt_segm.shape, "Segmentation mask dimensions don't match"
-    num_pixels = eval_segm.size
-    eval_segm.flatten()
-    gt_segm.flatten()
-    return np.sum(eval_segm == gt_segm)/num_pixels
+    comparison = (eval_segm == gt_segm).astype(int)
+    res = np.prod(comparison,axis=2)
+    return np.mean(res)
 
 
 #Test
-test_segm = np.random.randint(1034,size=(300,300,3))
-test_gt = np.random.randint(1034,size=(300,300,3))
+test_segm = np.random.randint(2,size=(300,300,3))
+test_gt = np.random.randint(2,size=(300,300,3))
 
 acc = pixel_accuracy(test_segm,test_gt)
 print("Accuracy",acc)
